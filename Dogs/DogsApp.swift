550ee3e1b816df7b457d82c0e2da610e84df2195
memokr <memo.kramsky@gmail.com>
2025-04-14 18:35:50 +0200

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Guillermo Kramsky on 14/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
